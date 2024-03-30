a7515fff4d52ba63c46e7cbbf2c82277aa6996a5
Antonino D'Anna <anda982008@gmail.com>
2024-03-30 13:17:54 +0100

    boh, ho ripreso solo oggi in mano il progetto, faccio un commit di sicurezza, pt2

---
+++ country.h
@@ -0,0 +1,23 @@
+#ifndef COUNTRY_ID
+#define COUNTRY_ID
+#include <string>
+#include <list>
+class Country{
+private:
+  const int id;
+  const std::list<int> hsc;
+  const std::string name;
+  std::list<int> sc;
+  std::list<int> this_turn_orders;
+  std::list<int> unit_location;
+  const int unit_number;
+
+public:
+  
+  
+  
+
+};
+
+
+#endif
---
+++ diplomacy.h
@@ -0,0 +1,4 @@
+#include "country.h"
+#include "global_variables.h"
+#include "region.h"
+
