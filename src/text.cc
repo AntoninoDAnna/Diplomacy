6926e1c0d8b3cccf11d78631c5d3cd2b93dac45b
Antonino D'Anna <anda982008@gmail.com>
2025-07-20 11:17:54 +0200

    resource manage and text are now only header files due to template usage, removed cc files

--- src/text.cc
+++
@@ -1,8 +0,0 @@
-#include "../include/text.h"
-#include "../include/log.h"
-#include "../include/resources_manager.h"
-#include <string>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-#include <iostream>
